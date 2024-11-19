2eb00c4a0b85a28cfefa61395575f41fc34a57e2
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-20 04:50:03 +0530

    Add files via upload

---
+++ Alexday8.py
@@ -0,0 +1,14 @@
+'''
+Author:Alex
+Date:20-11-2024
+Description: Write a program to identify and print all even and odd numbers within a specified range.
+'''
+num_1=int(input("Enter the start of the range"))
+num_2=int(input("Enter the end of the range"))
+for n in range(num_1,num_2+1):
+    if n%2==0 :
+        print(n,"is even")
+    elif n%2!=0 :
+        print(n,"is odd")
+    else:
+        print("Invalid Entry")
